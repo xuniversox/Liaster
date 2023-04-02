e1c2293cade7d06d5e53f5435d34a2c5fffb465e
Universo <tfgroses@gmail.com>
2023-04-02 02:35:13 +0100

    thanks to endless research with chatgpt, memorization and coding skills managed to get m3u support working with [][] filename syntax not sure how valid it is for other stuff yet; this project is going to take awhile to be universal that's for sure; will perhaps continue working on it

--- liaster-m3u/GetFileNames.rb
+++
@@ -1,8 +0,0 @@
-directory_list  = Dir.entries('.')
-
-sorted_directory_list = directory_list.sort_by {|directory_list| File.mtime(directory_list)}
-
-#puts shit
-
-puts "#EXTM3U"
-puts sorted_directory_list
--- liaster-m3u/wpm.bat
+++
@@ -1,4 +0,0 @@
-ruby GetFileNames.rb
-@echo off
-set /p variable=" FileName of TextFile? " 
-ruby GetFileNames.rb > %variable%.m3u
