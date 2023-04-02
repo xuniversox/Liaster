8ece037854483a760608f6e0cc0ee8c8eb1f962c
Dominic / Universa <67387763+xuniversox@users.noreply.github.com>
2023-04-02 02:58:49 +0100

    Update wpt.bat

--- liaster-txt/wpt.bat
+++ liaster-txt/wpt.bat
@@ -1,5 +1,5 @@
 ruby GetFileNamesTXT.rb
 @echo off
 set /p variable=" FileName of TextFile? " 
-ruby GetFileNames.rb > %variable%.txt
+ruby GetFileNamesTXT.rb > %variable%.txt
 mpv -ontop -playlist  %variable%.txt
